a21f88b78a1e939472031beb00e960eb83029433
NathanyM <naty_om_3@hotmail.com>
2022-02-21 11:41:27 -0300

    ProjetoKotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    println("Laís -Oi, tudo bem? ")
+    println("Leo - Tudo sim e com vc?")
+    println("Laís -O que vc aprendeu hoje na aula? ")
+    println("Leo - Eu aprendi sobre o Kotlin e como usar a plataforma IntelliJ")
+    println("Laís -Sério?? O que é o Kotlin? ")
+    println("Leo - Ah, é uma linguagem de programação")
+    println("Laís -Tipo o Java?")
+    println("Leo - Isso! É até derivada do Java, mas é utilizada em Android")
+    println("Laís -Ahh, que legal! E o que é IntelliJ?")
+    println("Leo - O IntelliJ é uma IDEA, tipo Vscode, sabe?")
+    println("Laís -Não... Eu só conheço o Portugol ainda")
+    println("Leo - Baixa aí e já começa a testar ")
+    println("Laís -Boa idéia! Vou fazer isso agora!")
+    println("Leo - Qualquer dúvida, me chama que eu te ajudo!")
+    println("Laís - Combinado! Muito obrigada! ")
+
+
+}
